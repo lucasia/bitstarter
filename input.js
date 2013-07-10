2920e1922de6202c2387c8f85ecbb33d17b40775
lucasia <ian.lucas@gmail.com>
2013-07-11 07:12:14 +0800

    Called buffer.toString

--- input.js
+++ input.js
@@ -2,6 +2,6 @@
 
 var fs = require('fs');
 var inputfile  = "index.html";
-var fileContent = fs.readFileSync(inputfile);
+var fileContent = fs.readFileSync(inputfile).toString();
 console.log("contents: " + fileContent)
 
